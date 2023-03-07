6723ffef3ef0fe47724b8e022591650a60d2640b
hyorimm <hyorimm@gmail.com>
2023-03-07 22:13:05 +0900

    feat: mock JSON 이용하여 메인에 정보 노출

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,33 @@
+import apiClient from '@/api/api';
+import { useState, useEffect } from 'react';
+
 const Main = () => {
-  return <div>Main</div>;
+  const [datas, setDatas] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await apiClient('');
+      setDatas(res.data);
+    };
+    getData();
+  }, []);
+
+  console.log(datas);
+
+  return (
+    <div>
+      {datas.map((data, i) => (
+        <div key={i}>
+          <div>{data.idx}</div>
+          <div>{data.name}</div>
+          <div>
+            <img src={data.mainImage} alt={data.name} />
+          </div>
+          <div>{data.price}</div>
+          <div>{data.spaceCategory}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Main;
