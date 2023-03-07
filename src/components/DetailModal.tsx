b3391a8c9aaf12a0b2ca535d40540303650678ec
hyorimm <hyorimm@gmail.com>
2023-03-08 01:05:16 +0900

    feat: 모달에 상품 정보 띄우기

---
+++ src/components/DetailModal.tsx
@@ -0,0 +1,39 @@
+import { Product } from '@/pages/MainPage';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  item: Product;
+}
+
+const DetailModal = ({ isOpen, onClose, item }: Props) => {
+  return (
+    <div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{item?.name}</ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+export default DetailModal;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,32 +1,69 @@
 import apiClient from '@/api/api';
 import { useState, useEffect } from 'react';
+import { TimeIcon } from '@chakra-ui/icons';
+import DetailModal from '@/components/DetailModal';
+import { Button, useDisclosure } from '@chakra-ui/react';
+
+export interface Product {
+  idx: number;
+  name: string;
+  mainImage: string;
+  description: string;
+  spaceCategory: string;
+  price: number;
+  maximumPurchases: number;
+  registrationDate: string;
+}
 
 const Main = () => {
-  const [datas, setDatas] = useState<any[]>([]);
+  const [data, setData] = useState<Product[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [index, setIndex] = useState(0);
+  const [cart, setCart] = useState({});
 
   useEffect(() => {
     const getData = async () => {
-      const res = await apiClient('');
-      setDatas(res.data);
+      const res = await apiClient({ method: 'get' });
+      setData(res.data);
     };
     getData();
   }, []);
 
-  console.log(datas);
+  const matchedData = data?.find((item) => item.idx === index);
 
+  console.log(index);
   return (
     <div>
-      {datas.map((data, i) => (
+      {data.map((item, i) => (
         <div key={i}>
-          <div>{data.idx}</div>
-          <div>{data.name}</div>
+          <div>{item.idx}</div>
+          <TimeIcon
+            cursor="pointer"
+            onClick={() => {
+              setIndex(item.idx);
+            }}
+          />
+          <div>{item.name}</div>
           <div>
-            <img src={data.mainImage} alt={data.name} />
+            <img src={item.mainImage} alt={item.name} />
           </div>
-          <div>{data.price}</div>
-          <div>{data.spaceCategory}</div>
+          <div>{item.price.toLocaleString()}원</div>
+          <div>{item.spaceCategory}</div>
+          <Button
+            onClick={() => {
+              setIndex(item.idx);
+              onOpen();
+            }}
+          >
+            더보기
+          </Button>
         </div>
       ))}
+      <DetailModal
+        isOpen={isOpen}
+        onClose={onClose}
+        item={matchedData as Product}
+      />
     </div>
   );
 };
