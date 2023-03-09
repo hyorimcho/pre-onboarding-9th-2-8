e63fb002cb54dcfdd3ec6dcfbed147e741ca3598
hyorimm <hyorimm@gmail.com>
2023-03-09 15:36:57 +0900

    feat: 저장한 여행 상품 삭제

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,5 +1,6 @@
 import { IProduct } from '@/interface/product';
 import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 const initialState: IProduct[] = [];
 
@@ -7,12 +8,36 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart: (state, action) => {
-      return [...state, action.payload];
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const itemInCart = state.find((item) => item.idx === action.payload.idx);
+      if (
+        itemInCart &&
+        itemInCart.quantity! <= action.payload.maximumPurchases
+      ) {
+        itemInCart.quantity! += action.payload.quantity as number;
+      } else {
+        state.push(action.payload);
+      }
+    },
+    incrementQuantity: (state, action: PayloadAction<IProduct>) => {
+      const itemInCart = state.find((item) => item.idx === action.payload.idx);
+      if (itemInCart && itemInCart.quantity! <= action.payload.maximumPurchases)
+        // itemInCart.length += 1;
+        console.log('itemInCart', itemInCart);
+    },
+    decrementQuantity: (state, action: PayloadAction<IProduct>) => {
+      const itemInCart = state.find((item) => item.idx === action.payload.idx);
+      if (itemInCart && itemInCart.quantity === 1) {
+        itemInCart.quantity = 1;
+      } else if (itemInCart) itemInCart.quantity! -= 1;
+    },
+    removeItem: (state, action: PayloadAction<IProduct>) => {
+      return state.filter((item) => item.idx !== action.payload.idx);
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, incrementQuantity, decrementQuantity, removeItem } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
